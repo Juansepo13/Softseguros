8a4c25050bb1e96388356c26a7542b6fd8a0f5ba
Juansepo13 <juancho_s1314@hotmail.com>
2023-06-08 15:21:12 -0500

    add all files

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets
 from .serializer import TaskSerializer
 from .models import Task
+from django.shortcuts import render
 
 # Create your views here.
 class TaskView(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+    
+def home(request):
+    return render(request, 'home.html')
