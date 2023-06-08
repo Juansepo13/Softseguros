46a28517facf20197ffb3fac2a2335e3fd673836
Juansepo13 <juancho_s1314@hotmail.com>
2023-06-08 15:21:55 -0500

    add all files

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 from rest_framework import routers
 from tasks import views
+from .views import home
+
 
 #api versioning
 router = routers.DefaultRouter()
@@ -10,4 +12,5 @@ router.register(r'tasks', views.TaskView, 'tasks')
 urlpatterns = [
     path("api/v1/", include(router.urls)),
     path('docs/', include_docs_urls(title='Tasks API')),
+    path('', home, name='home')
 ]
